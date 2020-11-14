7bbfa7b0516925ce2cd8c144a9cbfbc1174a2145
1234567980 <allison.li1324@gmail.com>
2020-11-13 21:57:12 -0500

    Added comments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from CLIRequestsAndJson.RequestMethods import *
 
+# Returns if a number is an valid input
 def valid_num_input(input):
     if input != "":
         try:
@@ -14,6 +15,7 @@ def valid_num_input(input):
             return "Please enter an integer"
     return "Please enter a non-empty integer"
 
+# Add a new pizza with a custom toppings/prep combo
 def add_custom():
     toppings = input("What toppings would you like on your new custom pizza?\n")
     pizzaName = input("What would you like to call your custom pizza?\n")
@@ -22,6 +24,7 @@ def add_custom():
 
     print(result.text)
 
+# Show an order
 def show_order():
     order_num = input("What is the number of the order you'd like to see?\n")
     check = valid_num_input(order_num)
@@ -33,6 +36,7 @@ def show_order():
 
         print(r.text)
 
+# Cancels an order
 def cancel_order():
     order_num = input("What is the number of the order you'd like to cancel?\n")
     check = valid_num_input(order_num)
@@ -43,10 +47,12 @@ def cancel_order():
         r = cancel_order_post(order_num)
         print(r.text)
 
+# Shows the price of a specific item
 def show_menu_item_get(item):
     json_string = show_menu_item_get(item)
     return requests.get('http://127.0.0.1:5000/menu/price', json=json_string)
 
+# Shows eveyrhting on the menu + cost
 def show_menu():
     view_menu = input("Would you like to view the entire menu?\n[1] Yes     [2] No\n")
     if (view_menu == "1"):
@@ -61,6 +67,7 @@ def show_menu():
     else:
         print ("Not a valid option")
 
+# Make a new order
 def order_pizzas_drinks():
     #Enter number of pizzas and drinks
     countedPizzas = 0
@@ -110,6 +117,10 @@ def order_pizzas_drinks():
 
                 print(result.text)
 
+# Updates a pizza in an order
+# Types of edit values: 1 -> Add new pizza
+#                       2 -> Delete pizza
+#                       3 -> Modify a pizza
 def update_pizzas(order_to_edit, types_of_edit):
     if (types_of_edit == "3"):
         num_pizza_to_update = input("Enter the pizza number you want to edit\n")
@@ -163,6 +174,10 @@ def update_pizzas(order_to_edit, types_of_edit):
 
             print(result.text)
 
+# Updates a drink in an order
+# Types of edit values: 1 -> Add new drink
+#                       2 -> Delete drink
+#                       3 -> Modify a drink
 def update_drinks(order_to_edit, types_of_edit):
     if (types_of_edit == "3"):
         num_drink_to_update = input("Enter the drink number you want to edit\n")
@@ -175,7 +190,6 @@ def update_drinks(order_to_edit, types_of_edit):
             result = modify_drink_post(order_to_edit, num_drink_to_update, new_drink)
             print(result.text)
 
-        # TODO add link and print results
     elif (types_of_edit == "1"):
         new_drink = input("New drink type? \n")
         result = add_drink_post(order_to_edit, new_drink)
@@ -191,6 +205,7 @@ def update_drinks(order_to_edit, types_of_edit):
             result = remove_drink_post(order_to_edit, drink_num)
             print(result.text)
 
+# Updates an order
 def update_order():
     while(True):
         update_pizza = input("Would you like to update an order?\n[1] Yes   [2] No\n")
@@ -216,6 +231,7 @@ def update_order():
         if (done == "1"):
             break
 
+# Change the price of somthing on the menu
 def change_price():
     item = input("Please enter item name who's price you want to change\n")
     r = show_menu_item_get(item)
@@ -228,6 +244,7 @@ def change_price():
         result = change_price_post(item, new_price)
         print(result.text)
 
+# Deliver an order
 def delivery(delivery_type):
     if (delivery_type == "Pickup"):
         print("Your order is on its way!")
@@ -243,6 +260,7 @@ def delivery(delivery_type):
             result = delivery_F_request(order_num, address)
             print(result.text)
 
+# Main client interface
 def cli():
     ordering = True
 
